3c8347e2002245b4d5c761605312641931e55560
divya <divyajainblc@gmail.com>
2026-02-26 23:57:30 +0530

    updated

--- web.py
+++ web.py
@@ -14,7 +14,6 @@ for index,todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
 st.text_input(label="",placeholder="Add new todo...",
               on_change=add_todo,key='new_todo') #to take input
 st.session_state
